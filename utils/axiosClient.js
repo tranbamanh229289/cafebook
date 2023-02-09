e9f992f26a00f9c0be37ae53b7dbb771df9c1a5e
son3102k <prokieumoi1@gmail.com>
2023-02-09 11:21:41 +0700

    fix conflix

--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosClient = (method, url, data, params) => {
+<<<<<<< HEAD
     const baseURL = "http://192.168.0.108:5000/it4788/";
+=======
+    const baseURL = 'http://192.168.0.101:5000/it4788/' 
+>>>>>>> b358421 (add secure storage , call api get_user_info)
     return axios({
         baseURL: baseURL,
         method: method,
